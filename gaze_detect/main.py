b421c18dff8c0fcc45eed754719aaa23a417e3be
linda46138 <linda46138@gmail.com>
2025-05-14 01:31:12 +0800

    gaze_detect upload
    gaze_detect

---
+++ gaze_detect/main.py
@@ -0,0 +1,13 @@
+import cv2
+from gaze_detect import predict_gaze_score
+
+# 讀取圖片
+img = cv2.imread('image.png')
+
+# 檢查圖片是否成功讀取
+if img is None:
+    print("Failed to load image")
+else:
+    # 計算並打印 Gaze Score
+    score = predict_gaze_score(img)
+    print("Gaze Score:", score)
